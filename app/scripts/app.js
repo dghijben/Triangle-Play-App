a8e4aee327b4ab50f087d772ffdb2eb10a89f2df
Nick Rowe <nixterrimus@dcxn.com>
2014-04-12 12:49:30 -0700

    Little webapp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,46 +8,9 @@ angular.module('trianglePlayAppApp', [
 ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .otherwise({
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
   });
 
-var width = document.body.clientWidth/1.5;
-var height = document.body.clientHeight/1.5;
-var t = new Trianglify({
-    x_gradient: colorbrewer.PuOr[9],
-    bleed: 200,
-    noiseIntensity: 0,
-    cellsize: 90});
-var pattern = t.generate(width, height);
-
-function convertDataURLToImageData(dataURL, callback) {
-    if (dataURL !== undefined && dataURL !== null) {
-        var canvas, context, image;
-        canvas = document.createElement('canvas');
-        canvas.width = width;
-        canvas.height = height;
-        context = canvas.getContext('2d');
-        image = new Image();
-        image.addEventListener('load', function(){
-            context.drawImage(image, 0, 0, canvas.width, canvas.height);
-            callback(canvas);
-        }, false);
-        image.src = dataURL;
-    }
-}
-
-convertDataURLToImageData(
-    pattern.dataUri,
-    function(canvas){
-      var image = canvas.toDataURL("image/png");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-      console.log(image.length);
-      var previewImage = document.getElementById('preview');
-      previewImage.src = image;
-    }
-)
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,52 @@
 
 angular.module('trianglePlayAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.trianglify = {
+      cellsize: 150,
+      bleed: 100,
+      cellpadding: 1,
+      noiseIntensity: 0,
+      palette: 'PuOr'
+    };
+    $scope.colorOptions = Object.keys(colorbrewer);
+    $scope.drawTrianglify = function(options){
+      var width = 600;
+      var height = 400;
+      var palette = colorbrewer[options.palette];
+      var t = new Trianglify({
+        x_gradient: palette[9],
+        bleed: parseInt(options.bleed, 10),
+        cellpadding: parseInt(options.cellpadding, 10),
+        noiseIntensity: parseFloat(options.noiseIntensity, 10),
+        cellsize: parseInt(options.cellsize, 10)
+      });
+      var pattern = t.generate(width, height);
+
+        function convertDataURLToImageData(dataURL, callback) {
+          if (dataURL !== undefined && dataURL !== null) {
+            var canvas, context, image;
+            canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            context = canvas.getContext('2d');
+            image = new Image();
+            image.addEventListener('load', function(){
+              context.drawImage(image, 0, 0, canvas.width, canvas.height);
+              callback(canvas);
+            }, false);
+            image.src = dataURL;
+          }
+        }
+
+        convertDataURLToImageData(
+          pattern.dataUri,
+          function(canvas){
+            var image = canvas.toDataURL("image/png");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+            console.log(image.length);
+            var previewImage = document.getElementById('preview');
+            previewImage.src = image;
+          }
+        );
+    };
+    $scope.drawTrianglify($scope.trianglify);
   });
